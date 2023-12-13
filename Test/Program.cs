9b298ddc31304d64c0f8652864c2d11832d44208
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-13 18:23:06 +0300

    home project

---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string text = "";
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                text += s[i];
+            }
+
+            if (text == s)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ tesr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tesr
+{
+    internal class Program
+    {
+        //12. Напишите программу, которая запрашивает у пользователя два числа
+        //    и выводит на экран все простые числа в этом диапазоне
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int countb = 0;
+            int counta = 0;
+            for(int i = 0; i < a; i++) 
+            {
+                if (a % 2 == 0) ;
+            }
+            counta++;
+
+            for (int i = 0; i < b; i++)
+            {
+                if (b % 2 == 0) ;
+            }
+            countb++;
+            Console.WriteLine(counta + countb);
+           
+        }
+    }
+}
